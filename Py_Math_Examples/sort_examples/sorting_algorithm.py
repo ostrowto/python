f1c6d4d32bc5dd862cc99b08a461c27e097acfec
Tomasz Ostrowski <ostrowto@gmail.com>
2019-08-31 22:39:04 +0200

    sorting_algorithm.py

---
+++ Py_Math_Examples/sort_examples/sorting_algorithm.py
@@ -0,0 +1,10 @@
+# sorting_algorithm.py
+
+def gnomesort(seq):
+    i = 0
+    while i < len(seq):
+        if i == 0 or seq[i-1] <= seq[i]:
+            i += 1
+        else:
+            seq[i], seq[i-1] = seq[i-1], seq[i] 
+            i -= 1
