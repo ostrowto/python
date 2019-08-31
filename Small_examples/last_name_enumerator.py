642d5d3f7d3f9b8013049fb3ab2832b7c5a91a19
Tomasz Ostrowski <ostrowto@gmail.com>
2019-08-31 21:52:29 +0200

    last_name_enumerator.py

---
+++ Small_examples/last_name_enumerator.py
@@ -0,0 +1,12 @@
+#last_name_enumerator.py
+
+lastName="Smith"
+count=0
+for letter in lastName:
+    print(letter," ",count)
+    count+=1
+print("-------")
+count=0
+while (count<5):
+    print(lastName[count]," ",count)
+    count+=1
