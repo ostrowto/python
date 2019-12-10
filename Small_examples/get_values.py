599e6fa8327ce4e1c937c0bf6df7616f76c1caa7
Tomasz Ostrowski <ostrowto@gmail.com>
2019-12-10 21:48:43 +0100

    get values

---
+++ Small_examples/get_values.py
@@ -0,0 +1,10 @@
+# get-values.py
+
+names_my = {
+    'First key' : 100,
+    'Seccond key' : 200,
+    'Third key' : 300,
+}
+
+getting_values = names_my.get('First key', 'nothing')
+print(getting_values) 
