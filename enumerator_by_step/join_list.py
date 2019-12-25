412d575b088308bfbb0a269b1b25cad762631006
Tomasz Ostrowski <ostrowto@gmail.com>
2019-12-25 15:36:14 +0100

    join list

---
+++ enumerator_by_step/join_list.py
@@ -0,0 +1,9 @@
+# join_list.py
+
+my_List = ['zero_element','first_element', 'second_element', 'third_element', 'fourth_element', 'fifth_element', 'sixth_element', 'seventh_element', 'eighth_element', 'nineth_element']
+
+my_List_As_String = ', '.join(my_List)
+
+print(my_List)
+print(20*"-")
+print(my_List_As_String)
