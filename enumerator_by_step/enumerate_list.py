c8a9341e427b2ea163b34143fbc0ebb123728333
Tomasz Ostrowski <ostrowto@gmail.com>
2019-12-25 15:24:59 +0100

    enumerate_list

---
+++ enumerator_by_step/enumerate_list.py
@@ -0,0 +1,6 @@
+# enumerate_list.py
+
+my_List = ['zero_element', 'first_element', 'second_element', 'third_element', 'fourth_element', 'fifth_element', 'sixth_element', 'seventh_element', 'eighth_element', 'ninth_element',]
+
+for index, my_List in enumerate(my_List, start = 0):
+    print(index, my_List)
