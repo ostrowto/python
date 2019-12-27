d50ef24c96f2b7fb5d18e8b16778119148148533
Tomasz Ostrowski <ostrowto@gmail.com>
2019-12-27 23:09:52 +0100

    2

---
+++ enumerator_by_step/enumerate.py
@@ -0,0 +1,6 @@
+# enumerate.py
+
+elements_on_list = ['el_0','el_1','el_2','el_3','el_4','el_5','el_6','el_7','el_8','el_9',]
+
+for i, el in enumerate(elements_on_list):
+    print(i, el)
---
+++ enumerator_by_step/enumerate_2.py
@@ -0,0 +1,7 @@
+# enumerate_2.py
+
+elements_on_list = [('el_0', 100), ('el_1', 200), ('el_2', 300), ('el_3', 400), ('el_4', 500), ('el_5', 600), ('el_6', 700), ('el_7', 800), ('el_8', 900), ('el_9', 1000)]
+
+for i, el in enumerate(elements_on_list):
+    print(i, ':', el)
+
