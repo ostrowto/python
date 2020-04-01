937a3cde6819e4cadc20030e6acbb8e74ad4a7d7
Tomasz Ostrowski <ostrowto@gmail.com>
2020-04-01 23:54:11 +0200

    quiz

---
+++ Small_examples/quiz/Question.py
@@ -0,0 +1,8 @@
+# Question.py
+# All rights Mike Dane https://www.youtube.com/watch?v=SgQhwtIoQ7o
+
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
