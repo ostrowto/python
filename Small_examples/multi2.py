02ad3d9846379c2d5bf22eac20bd10f391ada824
Tomasz Ostrowski <ostrowto@gmail.com>
2020-05-17 19:45:45 +0200

    multi2

---
+++ Small_examples/multi2.py
@@ -0,0 +1,48 @@
+
+
+num1 = int(input('Please your number 1? '))
+num2 = int(input('Please your number 2? '))
+print(20*'-')
+
+SUM = num1 + num2
+
+print('Addition: ', SUM, '   invoke: SUM')
+
+MULTI = num1 * num2
+
+print('Multiplication: ', MULTI, '   invoke: MULTI')
+
+
+DIVI = num1 / num2
+
+
+print('Division: ', DIVI, '   invoke: DIVI')
+
+SUB = num1 - num2
+
+print('Substraction: ', SUB, '   invoke: SUB')
+
+POW = num1 ** num2
+
+print('Power: ', POW, '   invoke: POW')
+
+DIVI_WHOLE = num1 // num2
+
+print('Division // : ', DIVI_WHOLE, '   invoke: DIVI_WHOLE')
+
+MOD = num1 % num2
+
+print('Modulo: ', MOD, '   invoke: MOD')
+
+first_CONC = str(num1) + str(num2)
+CONC = int(first_CONC)
+
+print('Concatenation of both numbers as integer: ', CONC, '   invoke: CONC')
+
+first_R_CONC = str(num2) + str(num1)
+R_CONC = int(first_R_CONC)
+
+print('Reverse concatenation of both numbers as integer: ', R_CONC, '   invoke: R_CONC')
+
+
+
